259c11b857044c6258e17b98d4f40db354649e93
Dalibor Janecek <dalibor.janecek@gmail.com>
2024-06-04 13:22:35 +0000

    timer demo

--- src/pages/TimerDemo.js
+++ src/pages/TimerDemo.js
@@ -4,23 +4,29 @@ import './TimerDemo.css';
 
 const TimerDemo = () => {
 
-    const [seconds, setSeconds] = useState(60);
+  const [seconds, setSeconds] = useState(60);
 
-    useEffect(() => {
-      if (seconds > 0) {
-        const intervalId = setInterval(() => {
-          setSeconds(prevSeconds => prevSeconds - 1);
-        }, 1000);
-  
-        // Vyčištění intervalu při odmountování komponenty nebo když seconds dosáhne 0
-        return () => clearInterval(intervalId);
-      } else {
-        setSeconds(60);
-      }
-    }, [seconds]);
+  useEffect(() => {
+    if (seconds > 0) {
+      const intervalId = setInterval(() => {
+        setSeconds(prevSeconds => prevSeconds - 1);
+      }, 1000);
+
+      // Vyčištění intervalu při odmountování komponenty nebo když seconds dosáhne 0
+      return () => clearInterval(intervalId);
+    } else {
+      setSeconds(60);
+    }
+  }, [seconds]);
 
   return (
-    <div className='timer'>{seconds}</div>
+    <div className="demo">
+      <div className='timer'>{seconds}</div>
+      <div className="controls">
+        <button className="controls--button" id='start'>start</button>
+        <button className="controls--button" id='reset'>reset</button>
+      </div>
+    </div>
   )
 }
 
