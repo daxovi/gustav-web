d99229bdd0efa0dd854d4b7e64ca40bcdf88b560
Dalibor Janeček <dalibor.janecek@gmail.com>
2024-06-05 22:11:18 +0200

    animation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
 import './App.css';
 import PhoneDemo from './components/PhoneDemo';
 import TimerDemo from './components/TimerDemo';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  const [xPosition, setXPosition] = useState(0)
+
+  const handleMouseMove = (e) => {
+    const { clientX, clientY } = e;
+    const { innerWidth, innerHeight } = window;
+
+    const offsetX = (clientX / innerWidth - 0.5) * 20; // Adjust the 20 to control sensitivity
+    const offsetY = (clientY / innerHeight - 0.5) * 20;
+
+    setXPosition(offsetX);
+    console.log({ x: offsetX, y: offsetY });
+  };
+
+  useEffect(() => {
+    window.addEventListener('mousemove', handleMouseMove);
+    return () => window.removeEventListener('mousemove', handleMouseMove);
+  }, []);
+  
   return (
     <div>
-      <div className="web">
+      <div className={xPosition < -2 ? "web web--animate-timer" : "web"}>
         <div className="web--menu">
           Menu
         </div>
