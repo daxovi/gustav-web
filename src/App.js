f4e86e29618a568e98386014fcfa4a9174a732f1
Dalibor Janecek <dalibor.janecek@gmail.com>
2024-06-06 14:06:51 +0000

    navigation

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import './App.css';
 import PhoneDemo from './components/PhoneDemo';
 import TimerDemo from './components/TimerDemo';
 import { useEffect, useState } from 'react';
+import appstore from "./img/appstore.svg";
+import QRCode from "react-qr-code";
 
 function App() {
 
-  const [xPosition, setXPosition] = useState(0)
+  const [xPosition, setXPosition] = useState(0);
+  const [selectedSection, setSelectedSection] = useState(0);
 
   const handleMouseMove = (e) => {
     const { clientX, clientY } = e;
@@ -15,17 +18,45 @@ function App() {
     const offsetY = (clientY / innerHeight - 0.5) * 20;
 
     setXPosition(offsetX);
-    console.log({ x: offsetX, y: offsetY });
+    console.log(offsetX);
   };
+
+  const animateClassName = () => {
+    if (selectedSection == 0) {
+      if (xPosition < -3) {
+        setSelectedSection(-1)
+        return "web web--animate-timer";
+      } else if (xPosition > 3) {
+        setSelectedSection(1)
+        return "web web--animate-weights";
+      } else {
+        return "web";
+      }
+    } else if (selectedSection == -1) {
+      if (xPosition > 8) {
+        setSelectedSection(1)
+        return "web web--animate-weights";
+      } else {
+        return "web web--animate-timer";
+      }
+    } else if (selectedSection == 1) {
+      if (xPosition < -8) {
+        setSelectedSection(-1)
+        return "web web--animate-timer";
+      } else {
+        return "web web--animate-weights";
+      }
+    }
+  }
 
   useEffect(() => {
     window.addEventListener('mousemove', handleMouseMove);
     return () => window.removeEventListener('mousemove', handleMouseMove);
   }, []);
-  
+
   return (
     <div>
-      <div className={xPosition < -2 ? "web web--animate-timer" : "web"}>
+      <div className={animateClassName()}>
         <div className="web--header">
           Header
         </div>
@@ -37,9 +68,15 @@ function App() {
           timer
         </div>
         <div className="web--demo">
-          <PhoneDemo />
+          <PhoneDemo selectedSection={selectedSection} />
           <div className="web--download">
-            Download on appstore
+            <a href="http://"><img src={appstore} alt="" srcset="" /></a>
+            <div className="web--download-qr">
+              <QRCode size={256}
+    style={{ height: "auto", maxWidth: "100%", width: "100%" }}
+    value="seznam.cz"
+    viewBox={`0 0 256 256`} />
+            </div>
           </div>
         </div>
         <div className="web--content web--weights">
--- src/components/PhoneDemo.js
+++ src/components/PhoneDemo.js
@@ -2,10 +2,10 @@ import React from 'react';
 import TimerDemo from './TimerDemo';
 import './PhoneDemo.css';
 
-const PhoneDemo = () => {
+const PhoneDemo = ({selectedSection}) => {
   return (
     <div className='phonedemo--timer'>
-      <div className="phonedemo--display">
+      <div className={selectedSection == 1 ? "phonedemo--display phonedemo--display-hidden" : "phonedemo--display"}>
       <TimerDemo /> 
       </div>
     </div>
